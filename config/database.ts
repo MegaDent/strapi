ea8b2b77f2e15ce705a04fd70121074bc32bf567
Sergiu333 <anghelenicis59@gmail.com>
2025-01-31 16:36:34 +0200

    s

--- config/database.ts
+++ config/database.ts
@@ -6,7 +6,7 @@ export default ({ env }) => {
   const connections = {
     mysql: {
       connection: {
-        host: env('DATABASE_HOST', 'localhost'),
+        host: env('DATABASE_HOST', 'ep-flat-moon-a2kfa6hg-pooler.eu-central-1.aws.neon.tech'),
         port: env.int('DATABASE_PORT', 3306),
         database: env('DATABASE_NAME', 'strapi'),
         user: env('DATABASE_USERNAME', 'strapi'),
